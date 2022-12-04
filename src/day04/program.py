5ff14ae1dc1c6e8e2cd1ee2dc2096db807dba1af
Keith Harshaw <keith.harshaw@gmail.com>
2022-12-04 08:42:17 -0500

    clean up.  looks prettier but takes more memory than original solution

--- src/day04/program.py
+++ src/day04/program.py
@@ -11,18 +11,16 @@ containments = 0
 overlaps = 0
 
 for p in pair:
-    elves = p.split(",")
+    assign1, assign2 = [[int(j) for j in i] for i in [x.split("-") for x in p.split(",")]]
 
-    elf1, elf2 = elves[0], elves[1]
-    assign1 = (int(elf1.split("-")[0]), int(elf1.split("-")[1]))
-    assign2 = (int(elf2.split("-")[0]), int(elf2.split("-")[1]))
+    area1 = set(range(assign1[0], assign1[1]+1))
+    area2 = set(range(assign2[0], assign2[1]+1))
 
-    if (assign1[0] >= assign2[0] and assign1[1] <= assign2[1] or 
-        assign2[0] >= assign1[0] and assign2[1] <= assign1[1]):
+
+    if area1.issubset(area2) or area1.issuperset(area2):
         containments += 1
         overlaps += 1
-    elif (assign1[1] in range(assign2[0], assign2[1]+1) or 
-        assign1[0] in range(assign2[0], assign2[1]+1)):
+    elif not area1.isdisjoint(area2):
         overlaps += 1
 
 print("p1: " + str(containments))
