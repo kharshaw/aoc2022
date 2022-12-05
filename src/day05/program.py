9ae6414ad1ed453965d7d87dfa7bcfc6403c16b3
Keith Harshaw <keith.harshaw@gmail.com>
2022-12-05 09:38:54 -0500

    more cleanup.  initialize() is still ugly

--- src/day05/program.py
+++ src/day05/program.py
@@ -7,36 +7,40 @@ with open ("./data.txt","r") as f:
 
 f.close()
 
-labelRe = re.compile("\s*\d\s*")
-moveRe = re.compile("\d+")
-crateRe = re.compile("[A-Z]")
 
-section = 0
-stacks = []
-crateData = []
-moves = list([])
-for line in data:
-    if section == 0:
-        if labelRe.match(line):
-            section = 1
-            for s in labelRe.findall(line):
-                stacks.append([])
-        else:
-            crates = [line[i:i+4] for i in range(0, len(line), 4)]
-            crateData.append(crates)
-    elif section == 1:
-        if len(line) == 0:
-            section = 2
-    elif section == 2:
-        (count, orig, dest) = moveRe.findall(line)
-        moves.append([int(count), int(orig), int(dest)])
+def initialize(data):
+    labelRe = re.compile("\s*\d\s*")
+    moveRe = re.compile("\d+")
+    crateRe = re.compile("[A-Z]")
 
-for c in crateData:
-    x = 0
-    for x in range(len(c)):
-        crate = crateRe.findall(c[x])
-        if len(crate) == 1:
-            stacks[x].append(crate[0])
+    section = 0
+    stacks = []
+    crateData = []
+    moves = list([])
+    for line in data:
+        if section == 0:
+            if labelRe.match(line):
+                section = 1
+                for s in labelRe.findall(line):
+                    stacks.append([])
+            else:
+                crates = [line[i:i+4] for i in range(0, len(line), 4)]
+                crateData.append(crates)
+        elif section == 1:
+            if len(line) == 0:
+                section = 2
+        elif section == 2:
+            (count, orig, dest) = moveRe.findall(line)
+            moves.append([int(count), int(orig), int(dest)])
+
+    for c in crateData:
+        x = 0
+        for x in range(len(c)):
+            crate = crateRe.findall(c[x])
+            if len(crate) == 1:
+                stacks[x].append(crate[0])
+
+    return (stacks, moves)
 
 def moveEm9000(stacks, moves):
     s = copy.deepcopy(stacks)
@@ -65,9 +69,9 @@ def getCode(stacks):
         code += s[0]
     return code
 
-print(stacks)
+(stacks, moves) = initialize(data)
+
 print("part 1 code: {c}".format(c = getCode(moveEm9000(stacks, moves))))
 
-print(stacks)
 print("part 2 code: {c}".format(c = getCode(moveEm9001(stacks, moves))))
 
