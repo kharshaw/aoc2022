674be7347597d4764aaf12560b17a97bd48936eb
Keith Harshaw <keith.harshaw@gmail.com>
2022-12-05 21:13:17 -0500

    refactored initialize to make it a little nicer

--- src/day05/program.py
+++ src/day05/program.py
@@ -1,6 +1,7 @@
 from functools import reduce
 import re
 import copy
+import math
 
 with open ("./data.txt","r") as f:
     data = f.read().splitlines()
@@ -8,39 +9,61 @@ with open ("./data.txt","r") as f:
 f.close()
 
 
-def initialize(data):
-    labelRe = re.compile("\s*\d\s*")
-    moveRe = re.compile("\d+")
-    crateRe = re.compile("[A-Z]")
+def initialize2():
+    with open ("./data.txt","r") as f:
+        data = f.read()
+    f.close()
 
-    section = 0
-    stacks = []
-    crateData = []
-    moves = list([])
-    for line in data:
-        if section == 0:
-            if labelRe.match(line):
-                section = 1
-                for s in labelRe.findall(line):
-                    stacks.append([])
-            else:
-                crates = [line[i:i+4] for i in range(0, len(line), 4)]
-                crateData.append(crates)
-        elif section == 1:
-            if len(line) == 0:
-                section = 2
-        elif section == 2:
-            (count, orig, dest) = moveRe.findall(line)
-            moves.append([int(count), int(orig), int(dest)])
+    moves = []
+    pack, instr = data.split("\n\n")
 
-    for c in crateData:
-        x = 0
-        for x in range(len(c)):
-            crate = crateRe.findall(c[x])
-            if len(crate) == 1:
-                stacks[x].append(crate[0])
+    # get packing layout
+    for idx, pl in enumerate(pack.splitlines()):
+        if idx == 0:  stacks = [list() for i in range(math.ceil(len(pl)/4))]
+
+        for idx, loc in enumerate(range(1, len(pl), 4)):
+            if (pl[loc] != " " and not pl[loc].isdigit()): stacks[idx].append(pl[loc])
+
+    for il in instr.splitlines():
+        (count, orig, dest) = re.findall("\d+", il)
+        moves.append([int(count), int(orig), int(dest)])
 
     return (stacks, moves)
+
+# def initialize(data):
+#     labelRe = re.compile("\s*\d\s*")
+#     moveRe = re.compile("\d+")
+#     crateRe = re.compile("[A-Z]")
+
+#     section = 0
+#     stacks = []
+#     crateData = []
+#     moves = list([])
+
+#     for line in data:
+#         if section == 0:
+#             if labelRe.match(line):
+#                 section = 1
+#                 for s in labelRe.findall(line):
+#                     stacks.append([])
+#             else:
+#                 crates = [line[i:i+4] for i in range(0, len(line), 4)]
+#                 crateData.append(crates)
+#         elif section == 1:
+#             if len(line) == 0:
+#                 section = 2
+#         elif section == 2:
+#             (count, orig, dest) = moveRe.findall(line)
+#             moves.append([int(count), int(orig), int(dest)])
+
+#     for c in crateData:
+#         x = 0
+#         for x in range(len(c)):
+#             crate = crateRe.findall(c[x])
+#             if len(crate) == 1:
+#                 stacks[x].append(crate[0])
+
+#     return (stacks, moves)
 
 def moveEm9000(stacks, moves):
     s = copy.deepcopy(stacks)
@@ -69,7 +92,8 @@ def getCode(stacks):
         code += s[0]
     return code
 
-(stacks, moves) = initialize(data)
+# (stacks, moves) = initialize(data)
+(stacks, moves) = initialize2()
 
 print("part 1 code: {c}".format(c = getCode(moveEm9000(stacks, moves))))
 
