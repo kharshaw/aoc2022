871e28d9fd8db06ba0a6c41f1fbe0a524bae24b6
Keith Harshaw <keith.harshaw@gmail.com>
2022-12-10 07:12:58 -0500

    day 9 part 1 working

--- src/day09/program.py
+++ src/day09/program.py
@@ -1,63 +1,65 @@
-from math import dist
+from functools import reduce
 
 with open("./data.txt","r") as f:
     data = f.read()
 f.close()
 
-def movetail(hx, hy, tx, ty, direction, visits):
+def movetail(head, tail, rope):
     
-    dx = hx - tx
-    dy = hy - ty
+    knot = 0
+    dx = rope[head]["x"] - rope[tail]["x"]
+    dy = rope[head]["y"] - rope[tail]["y"]
     #print("head at x:{x} y:{y}".format(x=hx,y=hy))
-    while abs(dx) > 1 or abs(dy) > 1:
+    if tail < len(rope):
+        while abs(dx) > 1 or abs(dy) > 1:
 
-        if (dx != 0 and dy != 0):
-            tx += dx//abs(dx)
-            ty += dy//abs(dy)
-        elif (dx != 0):
-            tx += dx//2
-        elif (dy !=0):
-            ty += dy//2
-    
-        #print ("move tail to x:{tx} y:{ty}".format(tx=tx,ty=ty))
-        visits.add("{x}:{y}".format(x=tx,y=ty))
+            if (dx != 0 and dy != 0):
+                rope[tail]["x"] += dx//abs(dx)
+                rope[tail]["y"] += dy//abs(dy)
+            elif (dx != 0):
+                rope[tail]["x"] += dx//2
+            elif (dy !=0):
+                rope[tail]["y"] += dy//2
         
-        dx = hx - tx
-        dy = hy - ty
+            rope[tail]["visits"].add("{x}:{y}".format(x=rope[tail]["x"],y=rope[tail]["y"]))
+            
+            dx = rope[head]["x"] - rope[tail]["x"]
+            dy = rope[head]["y"] - rope[tail]["y"]
+
+    movetail(head+1, tail+1, rope)    
+
+def initializeRope(knotCount):
+    rope = []
+    for i in range(0,knotCount):
+        rope.append({"x": 0, "y": 0, "visits": set("[0:0]")})
+    return rope
+
+def move(rope, moves):
+    for move in moves:
         
-    return (tx,ty)
+        (direction, distance) = move.split(" ")
+
+        for d in range(0,int(distance)):
+
+            if direction == "R":
+                rope[0]["x"] += 1
+            elif direction == "L":
+                rope[0]["x"] -= 1
+            elif direction == "U":
+                rope[0]["y"] += 1
+            elif direction == "D":
+                rope[0]["y"] -= 1
+
+            movetail(0, 1, rope)
+
 
 moves = data.splitlines()
-tx = 0
-ty = 0
 
-hx = 0
-hy = 0
+rope1 = initializeRope(2)
 
-visits = set(["0:0"])
+move(rope1, moves)
 
-for move in moves:
-       
-    (direction, distance) = move.split(" ")
+print("p1: {p1}".format(p1 = reduce(lambda acc, val: acc + len(val["visits"]), rope1)))
 
-    #print(direction, distance)
-   
-    for d in range(0,int(distance)):
-
-        if direction == "R":
-            hx += 1
-        elif direction == "L":
-            hx -= 1
-        elif direction == "U":
-            hy += 1
-        elif direction == "D":
-            hy -= 1
-
-        (tx,ty) = movetail(hx, hy, tx,ty, direction, visits)
-
-    #print(sorted(visits))
-
-
-print(len(visits))
 
 
